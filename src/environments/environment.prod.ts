48450865ddef86c9c52992b6f61a2e142afaf086
Thor Remiendo <thorremiendo@gmail.com>
2024-01-18 22:56:42 +0800

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDyGA1eSyRPhdrahwFGxdJoV0iT_MbBZs4",
+    authDomain: "thor-aia-wedding.firebaseapp.com",
+    projectId: "thor-aia-wedding",
+    storageBucket: "thor-aia-wedding.appspot.com",
+    messagingSenderId: "389440660496",
+    appId: "1:389440660496:web:7f4d42a765b63f93e9d695",
+    measurementId: "G-Y388XJRF2B"
+  }
 };
